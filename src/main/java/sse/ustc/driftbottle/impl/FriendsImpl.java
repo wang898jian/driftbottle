be98a9d2205f91296463a08243f3ed7291559acc
wang898jian <wang898jian@163.com>
2015-05-28 01:03:36 +0800

    使用url直接定位accessory
    用get方法直接获取图像文件，
    
    localhost:8080/driftBottle/Friends/myresource/0E2A539E-203B-4B55-885B-8FFE73EF001A.JPG
    void getAccessory

--- src/main/java/sse/ustc/driftbottle/impl/FriendsImpl.java
+++ src/main/java/sse/ustc/driftbottle/impl/FriendsImpl.java
@@ -1,6 +1,7 @@
 package sse.ustc.driftbottle.impl;
-
+import javax.servlet.ServletContext;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
@@ -9,14 +10,21 @@ import java.io.OutputStream;
 import java.util.List;
 import java.util.Random;
 
+import javax.activation.MimetypesFileTypeMap;
 import javax.persistence.Entity;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServletResponse;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.FormParam;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 import sse.ustc.driftbottle.DAO.Accessory;
 import sse.ustc.driftbottle.DAO.AccessoryDAO;
@@ -34,47 +42,46 @@ import com.sun.jersey.core.header.FormDataContentDisposition;
 import com.sun.jersey.multipart.FormDataMultiPart;
 import com.sun.jersey.multipart.FormDataParam;
 import com.sun.jersey.multipart.file.FileDataBodyPart;
-
 @Entity
 @Path("/myresource")
 public class FriendsImpl {
 	// TODO: update the class to suit your needs
 	// The Java method will process HTTP GET requests
-//	@GET
-//	// The Java method will produce content identified by the MIME Media
-//	@Produces({ MediaType.APPLICATION_JSON })
-//	public Friends getFriends() {
-//		Friends a = new Friends();
-//		// 存库的实现
-//		FriendsDAO aDao = new FriendsDAO();
-//		aDao.save(a);
-//		return a;
-//	}
-//
+	// @GET
+	// // The Java method will produce content identified by the MIME Media
+	// @Produces({ MediaType.APPLICATION_JSON })
+	// public Friends getFriends() {
+	// Friends a = new Friends();
+	// // 存库的实现
+	// FriendsDAO aDao = new FriendsDAO();
+	// aDao.save(a);
+	// return a;
+	// }
+	//
 
-//
-//	@GET
-//	@Path("/226ddd")
-//	@Produces({ MediaType.TEXT_PLAIN })
-//	public String getString() {
-//		String userName = "nam5";
-//		String passwd = "123456";
-//		System.out.println(userName);
-//		System.out.println(passwd);
-//		Loginformation logInfo = new Loginformation();
-//		logInfo.setUserName(userName);
-//		logInfo.setPassWd(passwd);
-//		LoginformationDAO logInforDao = new LoginformationDAO();
-//		UserinfoDAO relatinfoDAO = new UserinfoDAO();
-//		Userinfo relatinfo = new Userinfo();
-//		relatinfo.setUserName(userName);
-//		if (logInforDao.findByProperty("userName", userName).isEmpty()) {
-//			relatinfoDAO.save(relatinfo);
-//			logInforDao.save(logInfo);
-//			return relatinfo.getUserId().toString();
-//		} else
-//			return "false";
-//	}
+	//
+	// @GET
+	// @Path("/226ddd")
+	// @Produces({ MediaType.TEXT_PLAIN })
+	// public String getString() {
+	// String userName = "nam5";
+	// String passwd = "123456";
+	// System.out.println(userName);
+	// System.out.println(passwd);
+	// Loginformation logInfo = new Loginformation();
+	// logInfo.setUserName(userName);
+	// logInfo.setPassWd(passwd);
+	// LoginformationDAO logInforDao = new LoginformationDAO();
+	// UserinfoDAO relatinfoDAO = new UserinfoDAO();
+	// Userinfo relatinfo = new Userinfo();
+	// relatinfo.setUserName(userName);
+	// if (logInforDao.findByProperty("userName", userName).isEmpty()) {
+	// relatinfoDAO.save(relatinfo);
+	// logInforDao.save(logInfo);
+	// return relatinfo.getUserId().toString();
+	// } else
+	// return "false";
+	// }
 
 	@POST
 	@Path("/sendBottle")
@@ -124,39 +131,39 @@ public class FriendsImpl {
 		return "true";
 	}
 
-//	@GET
-//	@Path("/sendMessage")
-//	@Produces(MediaType.TEXT_PLAIN)
-//	// bottleID messageID text senderUserID
-//	public String sendMassageTmp() {
-//		Message message = new Message();
-//		message.setBottleId("78565");
-//		message.setMessageId("15132151");
-//		message.setText("21351231");
-//		message.setSenderUserId(7);
-//		System.out.print(message.getText());
-//		System.out.println("ooooaaaa");
-//		BottleDAO bottleDAO = new BottleDAO();
-//		Bottle bottle;
-//		if (bottleDAO.findByProperty("bottleId", message.getBottleId())
-//				.isEmpty()) {
-//			return "can't find the bottle!";
-//		} else {
-//			bottle = (Bottle) bottleDAO.findByProperty("bottleId",
-//					message.getBottleId()).get(0);
-//		}
-//		if (bottle.getId() == null) {
-//			System.out.println("132151321");
-//		}
-//		System.out.println(bottle.getId());
-//		System.out.println(bottle.getBottleId());
-//		message.setBottle(bottle);
-//		System.out.println(message.getBottle().getBottleId());
-//		bottleDAO.attachDirty(bottle);
-//		MessageDAO messageDAO = new MessageDAO();
-//		messageDAO.attachDirty(message);
-//		return "true";
-//	}
+	// @GET
+	// @Path("/sendMessage")
+	// @Produces(MediaType.TEXT_PLAIN)
+	// // bottleID messageID text senderUserID
+	// public String sendMassageTmp() {
+	// Message message = new Message();
+	// message.setBottleId("78565");
+	// message.setMessageId("15132151");
+	// message.setText("21351231");
+	// message.setSenderUserId(7);
+	// System.out.print(message.getText());
+	// System.out.println("ooooaaaa");
+	// BottleDAO bottleDAO = new BottleDAO();
+	// Bottle bottle;
+	// if (bottleDAO.findByProperty("bottleId", message.getBottleId())
+	// .isEmpty()) {
+	// return "can't find the bottle!";
+	// } else {
+	// bottle = (Bottle) bottleDAO.findByProperty("bottleId",
+	// message.getBottleId()).get(0);
+	// }
+	// if (bottle.getId() == null) {
+	// System.out.println("132151321");
+	// }
+	// System.out.println(bottle.getId());
+	// System.out.println(bottle.getBottleId());
+	// message.setBottle(bottle);
+	// System.out.println(message.getBottle().getBottleId());
+	// bottleDAO.attachDirty(bottle);
+	// MessageDAO messageDAO = new MessageDAO();
+	// messageDAO.attachDirty(message);
+	// return "true";
+	// }
 
 	@POST
 	@Path("/loginUser")
@@ -214,12 +221,11 @@ public class FriendsImpl {
 		System.out.println(accessoryId);
 		System.out.println(messageId);
 		int i = 10;
-		System.out.println("0"+i);
-		
+		System.out.println("0" + i);
+
 		try {
 			OutputStream outputStream = new FileOutputStream(new File(
-					"C:\\upload", accessoryId
-							+ expandName));
+					"C:\\upload", accessoryId + expandName));
 			int length = 0;
 
 			byte[] buff = new byte[256];
@@ -238,16 +244,16 @@ public class FriendsImpl {
 			// e.printStackTrace();
 			return e.toString();
 		}
-		
-		//没有异常，逻辑存库
+
+		// 没有异常，逻辑存库
 		Accessory accessory = new Accessory();
 		accessory.setAccessoryId(accessoryId);
 		accessory.setAccessoryType(expandName);
 		MessageDAO messageDAO = new MessageDAO();
-		Message message = (Message)messageDAO.
-				findByProperty("messageId", messageId).get(0);
+		Message message = (Message) messageDAO.findByProperty("messageId",
+				messageId).get(0);
 		if (message.isEmpty()) {
-			System.out.println("can't find message, ID:"+messageId);
+			System.out.println("can't find message, ID:" + messageId);
 			return "can't find message";
 		}
 		messageDAO.attachDirty(message);
@@ -264,28 +270,56 @@ public class FriendsImpl {
 			@FormParam("messageId") String messageId) {
 		AccessoryDAO accessoryDAO = new AccessoryDAO();
 		FormDataMultiPart formDataMultiPart = new FormDataMultiPart();
-		List<Accessory> accessories = accessoryDAO.findByProperty("messageId", messageId);
+		List<Accessory> accessories = accessoryDAO.findByProperty("messageId",
+				messageId);
 		Integer i = 0;
 		for (; i < accessories.size(); i++) {
 			Accessory accessoryTmp = accessories.get(i);
 			File tmpFile = new File("C:\\upload", accessoryTmp.getAccessoryId()
-							+accessoryTmp.getAccessoryType());
-			FileDataBodyPart TmpBodyPart = new FileDataBodyPart(i.toString(), tmpFile);
+					+ accessoryTmp.getAccessoryType());
+			FileDataBodyPart TmpBodyPart = new FileDataBodyPart(i.toString(),
+					tmpFile);
 			formDataMultiPart.bodyPart(TmpBodyPart);
 		}
 		formDataMultiPart.field("numbOfFile", i.toString());
 		return formDataMultiPart;
 	}
+
+	@GET
+	@Path("{imageName}")
+	@Produces(MediaType.APPLICATION_OCTET_STREAM)
+	public void getAccessory(@Context HttpServletResponse response,
+			@PathParam("imageName") String imageName) throws Exception{
+	File pathsavefile = new File("C:\\upload", imageName);
+	response.reset(); 
+	response.setContentType("APPLICATION/OCTET-STREAM"); 
+	imageName=response.encodeURL(new String(imageName.getBytes(),"UTF-8"));//转码 
+	response.setHeader("Content-Disposition", "attachment; filename=\""+imageName+"\"");
+	ServletOutputStream out = response.getOutputStream(); 
+	InputStream inStream=new FileInputStream(pathsavefile); 
+	byte[] b = new byte[1024]; 
+	int len; 
+	while((len=inStream.read(b)) >0) 
+	out.write(b,0,len); 
+	response.setStatus( response.SC_OK ); 
+	response.flushBuffer(); 
+	out.close(); 
+	inStream.close();
+	return;
+	}
+	
 	
 	@POST
 	@Path("/getMessage")
 	@Produces(MediaType.APPLICATION_JSON)
-	public List<Message> getMessage(
-			@FormParam("bottleId") String bottleId) {
+	public List<Message> getMessage(@FormParam("bottleId") String bottleId) {
 		MessageDAO messageDAO = new MessageDAO();
-		List<Message> messages = messageDAO.findByProperty("bottleId", bottleId);
+		List<Message> messages = messageDAO
+				.findByProperty("bottleId", bottleId);
 		return messages;
 	}
+
+	
 	
 	@POST
 	@Path("/getUserBottle")
@@ -293,15 +327,15 @@ public class FriendsImpl {
 	public List<Bottle> getUserBottle(
 			@FormParam("senderUserId") String senderUserId) {
 		BottleDAO bottleDAO = new BottleDAO();
-		List<Bottle> bottles = bottleDAO.findByProperty("senderUserId", senderUserId);
+		List<Bottle> bottles = bottleDAO.findByProperty("senderUserId",
+				senderUserId);
 		return bottles;
 	}
-	
+
 	@POST
 	@Path("/getRandomBottle")
 	@Produces(MediaType.APPLICATION_JSON)
-	public Bottle getRandomBottle(
-			@FormParam("userId") String userId) {
+	public Bottle getRandomBottle(@FormParam("userId") String userId) {
 		BottleDAO bottleDAO = new BottleDAO();
 		List<Bottle> bottles = bottleDAO.findByProperty("userId", "");
 		Integer sizeInteger = bottles.size();
@@ -309,16 +343,17 @@ public class FriendsImpl {
 		Bottle bottle;
 		Integer numb = 0;
 		do {
-			Integer tmpInteger = Math.abs(random.nextInt()%sizeInteger);
+			Integer tmpInteger = Math.abs(random.nextInt() % sizeInteger);
 			bottle = bottles.get(tmpInteger);
 			numb++;
-		} while (bottle.getUserinfoBySenderUserId().getUserId().equals(userId)||numb<sizeInteger);
-        if (numb>sizeInteger) {
-        	bottle.setId(-1);
+		} while (bottle.getUserinfoBySenderUserId().getUserId().equals(userId)
+				|| numb < sizeInteger);
+		if (numb > sizeInteger) {
+			bottle.setId(-1);
 		}
 		return bottle;
 	}
-	
+
 	@GET
 	@Path("/list")
 	@Produces({ MediaType.MULTIPART_FORM_DATA })
@@ -335,6 +370,7 @@ public class FriendsImpl {
 		a1.bodyPart(c);
 		return a1;
 	}
+
 	@POST
 	@Path("/registUser")
 	@Produces(MediaType.TEXT_PLAIN)
